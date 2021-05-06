2002cead5b061ac698d362c1c5067ab4c0e3ad09
Joshua Fritsch <29730066+jfritsch75@users.noreply.github.com>
2021-05-06 07:46:45 -0400

    Improved flow

--- PY04/except.py
+++ PY04/except.py
@@ -33,24 +33,29 @@ while value1 != "X":
             breakthat = 100 / value1
         elif value1 == "C":
             print("https://docs.python.org/3/library/exceptions.html\n")
+            exit(0)
         elif value1 == "F":
             # You can "raise" an exception from anywhere!
             raise Exception("This is a custom message applied to the exception you just forced.\n")
+            exit(4)
         else:
             pass
 
     # Print a custom message when we trigger a ZeroDivisionError
     except ZeroDivisionError:
         print("This is a custom message applied to all ZeroDivisionError exceptions.\n")
+        exit(1)
 
     # Print a custom message when we trigger a TypeError
     except TypeError:
         print("***This is a custom message applied to all TypeError exceptions.\n")
+        exit(2)
 
     # Print a custom message for any other Exception types not already
     # explicitly defined.
     except Exception:
         print("***This is a custom message applied to any exception that is not a ZeroDivisionError or TypeError.\n")
+        exit(3)
 
     # Explicitly confirm that no exceptions occurred
     else:
